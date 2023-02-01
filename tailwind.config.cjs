00d572c4a4d6fe940b9aad8d5ec3b5ca0c8e775d
Valley <vaaleyou@gmail.com>
2023-02-01 19:10:47 +0100

    almost finished v0.5

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,6 +10,17 @@ module.exports = {
         '-md': { max: '767px' },
         '-sm': { max: '639px' },
       },
+      colors: {
+        'bright-orange': 'hsl(31, 77%, 52%)',
+        'dark-cyan': 'hsl(184, 100%, 22%)',
+        'very-dark-cyan': 'hsl(179, 100%, 13%)',
+        'transparent-white': 'hsla(0, 0%, 100%, 0.75)',
+        'very-light-grey': 'hsl(0, 0%, 95%)',
+      },
+      fontFamily: {
+        'big-shoulders': ['Big Shoulders Display', 'sans-serif'],
+        'lexend-deca': ['Lexend Deca', 'sans-serif'],
+      },
     },
   },
   plugins: [],
